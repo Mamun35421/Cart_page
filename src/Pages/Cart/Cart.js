43d7447e265eb0ad7d3a9efbdf3cfe44eafc9104
Mamun Or Rashid <86666104+Mamun35421@users.noreply.github.com>
2023-02-08 03:31:55 +0600

    footer add

---
+++ src/Pages/Cart/Cart.js
@@ -0,0 +1,170 @@
+import React from 'react'
+import './Cart.css'
+import 'bootstrap/dist/css/bootstrap.css';
+
+const Cart = () => {
+  return (
+    <div>
+      <section class="h-100">
+        <h1 style={{textAlign:'center'}}>My cart</h1>
+        <div class="container h-100 py-5">
+          <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-10">
+
+            <div class="card  mb-4">
+                <div class="card-body p-4">
+                  <div class="row d-flex justify-content-between align-items-center">
+                    <div class="col-md-2 col-lg-2 col-xl-2">
+                      <img
+                        src="https://product-landing-page-vert.vercel.app/images/hero_img.png"
+                        class="img-fluid rounded-3" alt="Cotton T-shirt" />
+                    </div>
+                    <div class="col-md-3 col-lg-3 col-xl-3">
+                      <p class="text-lg-start"><h6>Apple watch serise 2</h6></p>
+                      <p class="text-sm-start"><span>3000BDT</span> </p>
+                      <p class="text-sm-start"><span>Shop name</span></p>
+
+                    </div>
+
+                    <div class="col-md-3 col-lg-2 col-xl-2 ">
+                      <h5 class="lead fw-normal mb-2">500bdt</h5>
+                    </div>
+                    <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
+                      <p className='lead fw-normal mb-2'>+ <span>1</span> <span>-</span></p>
+                    </div>
+
+                    <div class="col-md-1 col-lg-1 col-xl-1 text-end">
+                      <a href="#!" class="text-danger"><i class="fas fa-trash fa-lg"></i></a>
+                    </div>
+                  </div>
+                </div>
+                
+              </div>
+              
+            <div class="card  mb-4">
+                <div class="card-body p-4">
+                  <div class="row d-flex justify-content-between align-items-center">
+                    <div class="col-md-2 col-lg-2 col-xl-2">
+                      <img
+                        src="https://product-landing-page-vert.vercel.app/images/hero_img.png"
+                        class="img-fluid rounded-3" alt="Cotton T-shirt" />
+                    </div>
+                    <div class="col-md-3 col-lg-3 col-xl-3">
+                      <p class="text-lg-start"><h6>Apple watch serise 2</h6></p>
+                      <p class="text-sm-start"><span>3000BDT</span> </p>
+                      <p class="text-sm-start"><span>Shop name</span></p>
+
+                    </div>
+
+                    <div class="col-md-3 col-lg-2 col-xl-2 ">
+                      <h5 class="lead fw-normal mb-2">500bdt</h5>
+                    </div>
+                    <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
+                      <p className='lead fw-normal mb-2'>+ <span>1</span> <span>-</span></p>
+                    </div>
+
+                    <div class="col-md-1 col-lg-1 col-xl-1 text-end">
+                      <a href="#!" class="text-danger"><i class="fas fa-trash fa-lg"></i></a>
+                    </div>
+                  </div>
+                </div>
+                
+              </div>
+              
+            <div class="card  mb-4">
+                <div class="card-body p-4">
+                  <div class="row d-flex justify-content-between align-items-center">
+                    <div class="col-md-2 col-lg-2 col-xl-2">
+                      <img
+                        src="https://product-landing-page-vert.vercel.app/images/hero_img.png"
+                        class="img-fluid rounded-3" alt="Cotton T-shirt" />
+                    </div>
+                    <div class="col-md-3 col-lg-3 col-xl-3">
+                      <p class="text-lg-start"><h6>Apple watch serise 2</h6></p>
+                      <p class="text-sm-start"><span>3000BDT</span> </p>
+                      <p class="text-sm-start"><span>Shop name</span></p>
+
+                    </div>
+
+                    <div class="col-md-3 col-lg-2 col-xl-2 ">
+                      <h5 class="lead fw-normal mb-2">500bdt</h5>
+                    </div>
+                    <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
+                      <p className='lead fw-normal mb-2'>+ <span>1</span> <span>-</span></p>
+                    </div>
+
+                    <div class="col-md-1 col-lg-1 col-xl-1 text-end">
+                      <a href="#!" class="text-danger"><i class="fas fa-trash fa-lg"></i></a>
+                    </div>
+                  </div>
+                </div>
+                
+              </div>
+              
+            <div class="card  mb-4">
+                <div class="card-body p-4">
+                  <div class="row d-flex justify-content-between align-items-center">
+                    <div class="col-md-2 col-lg-2 col-xl-2">
+                      <img
+                        src="https://product-landing-page-vert.vercel.app/images/hero_img.png"
+                        class="img-fluid rounded-3" alt="Cotton T-shirt" />
+                    </div>
+                    <div class="col-md-3 col-lg-3 col-xl-3">
+                      <p class="text-lg-start"><h6>Apple watch serise 2</h6></p>
+                      <p class="text-sm-start"><span>3000BDT</span> </p>
+                      <p class="text-sm-start"><span>Shop name</span></p>
+
+                    </div>
+
+                    <div class="col-md-3 col-lg-2 col-xl-2 ">
+                      <h5 class="lead fw-normal mb-2">500bdt</h5>
+                    </div>
+                    <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
+                      <p className='lead fw-normal mb-2'>+ <span>1</span> <span>-</span></p>
+                    </div>
+
+                    <div class="col-md-1 col-lg-1 col-xl-1 text-end">
+                      <a href="#!" class="text-danger"><i class="fas fa-trash fa-lg"></i></a>
+                    </div>
+                  </div>
+                </div>
+                
+              </div>
+          
+           
+          
+
+
+
+
+
+              <div class=" mb-4">
+                <div class="card mb-4">
+                </div>
+                <div class="col-md-3 col-lg-3 col-xl-3">
+                  <p className='total'>Subtotal </p>
+                </div>
+                <div class="col-md-3 col-lg-3 col-xl-3">
+                  <p className='taka'>Shipping </p>
+                </div>
+
+                <div class="card mb-4">
+                </div>
+                <div class="col-md-3 col-lg-3 col-xl-3">
+                  <p className='total'>Total </p>
+                </div>
+                <button className=" btn btn-warning btn-block btn-lg  ">Proceed to Pay</button>
+
+              </div>
+
+
+
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Cart
